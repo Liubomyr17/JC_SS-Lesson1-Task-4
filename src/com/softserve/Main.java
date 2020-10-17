99b481c248081819419b419e566a548b9803a8b5
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-10-18 01:42:24 +0300

    Add files via upload

---
+++ src/com/softserve/Main.java
@@ -0,0 +1,14 @@
+package com.softserve;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 2;
+        double c = 2.5;
+        float result = (float) (a / (double) b * c);
+        System.out.println(result);  // 6.25
+}
+}
+
+
